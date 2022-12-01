5f1b19622a82282cc2eb6253da80cf8337db7818
Callum Hutchison <callumhutch98@gmail.com>
2022-12-01 09:37:15 -0500

    Day 1

---
+++ advent_of_code_elixir/lib/advent_of_code_2022.ex
@@ -0,0 +1,33 @@
+defmodule AdventOfCode2022 do
+  alias AdventOfCode2022
+
+  @solutions [
+    %{day: "01", title: "Calorie Counting", function: &AdventOfCode2022.Day01.run/1}
+  ]
+
+  @table_mapping [
+    {"Day", :day},
+    {"Title", :title},
+    {"Part 1 Answer", :part1},
+    {"Part 2 Answer", :part2},
+    {"Total execution time (s)", :time}
+  ]
+
+  def run do
+    @solutions
+    |> Enum.map(&Task.async(fn -> time_task(&1) end))
+    |> Enum.map(&Task.await(&1, 10000))
+    |> Scribe.print(data: @table_mapping)
+
+    :ok
+  end
+
+  defp time_task(%{day: day, title: title, function: nil}) do
+    %{day: day, title: title, part1: "n/a - Done in JS", part2: "n/a - Done in JS", time: "n/a"}
+  end
+
+  defp time_task(%{day: day, title: title, function: function}) do
+    {time, {part1, part2}} = :timer.tc(function, ["input"])
+    %{day: day, title: title, part1: part1, part2: part2, time: time / 1_000_000}
+  end
+end
---
+++ advent_of_code_elixir/lib/day01/day01.ex
@@ -0,0 +1,19 @@
+defmodule AdventOfCode2022.Day01 do
+  def run(input_file_name) do
+    input = load_input(input_file_name)
+    {calculate_highest_calories(input), calculate_highest_calories(input, 3)}
+  end
+
+  def load_input(file_name) do
+    File.read!("lib/day01/#{file_name}.txt")
+    |> String.split(~r/\R\R/)
+    |> Enum.map(fn lines -> String.split(lines, ~r/\R/) |> Enum.map(&String.to_integer/1) end)
+  end
+
+  def calculate_highest_calories(input, count \\ 1) do
+    Enum.map(input, &Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(count)
+    |> Enum.sum()
+  end
+end
