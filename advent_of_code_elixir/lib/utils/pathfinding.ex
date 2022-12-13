71c6f220016e03c2ce1acfb0ec09627dc6ab1406
Callum Hutchison <callumhutch98@gmail.com>
2022-12-12 20:28:04 -0500

    Day 12

---
+++ advent_of_code_elixir/lib/utils/pathfinding.ex
@@ -0,0 +1,56 @@
+defmodule AdventOfCode2022.Utils.Pathfinding do
+  def get_path(start, target, connections) do
+    get_path(target, connections, [%{position: start, distance: 0, parent: nil}], MapSet.new())
+  end
+
+  defp get_path(_target, _connections, [], _closed_set) do
+    :unreachable
+  end
+
+  defp get_path(target, _connections, [node | _], _closed_set) when node.position == target,
+    do: retrace_path(node)
+
+  defp get_path(target, connections, [node | open_queue], closed_set) do
+    queue =
+      Enum.filter(
+        connections[node.position],
+        &(!MapSet.member?(closed_set, elem(&1, 0)))
+      )
+      |> Enum.reduce(open_queue, fn
+        {pos, cost}, acc ->
+          case Enum.find(acc, &(&1.position == pos)) do
+            %{distance: current_distance} when current_distance <= node.distance + cost ->
+              acc
+
+            nil ->
+              add_node(acc, %{
+                position: pos,
+                distance: node.distance + cost,
+                parent: node
+              })
+
+            existing ->
+              List.delete(acc, existing)
+              |> add_node(%{
+                position: pos,
+                distance: node.distance + cost,
+                parent: node
+              })
+          end
+      end)
+
+    get_path(target, connections, queue, MapSet.put(closed_set, node.position))
+  end
+
+  defp add_node([], node), do: [node]
+
+  defp add_node([%{distance: head_dist} | _] = queue, %{distance: new_dist} = node)
+       when new_dist < head_dist,
+       do: [node | queue]
+
+  defp add_node([head | tail], node),
+    do: [head | add_node(tail, node)]
+
+  defp retrace_path(nil), do: []
+  defp retrace_path(%{position: pos, parent: parent}), do: [pos | retrace_path(parent)]
+end
